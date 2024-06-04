fc619a1fee733280a5f0a5d5c2875f59786311f0
Mahedi205339 <mahedi205339@gmail.com>
2024-06-04 21:41:20 +0600

    added some layout related code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper className="pb-24 pt-10 flex lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="absolute w-28 left-0 -top-20 hidden lg:block">
 
+            </div>
+          </div>
+        </MaxWidthWrapper>
+      </section>
     </div>
   );
 }
