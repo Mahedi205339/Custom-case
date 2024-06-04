787d5721e864c5532a0589b97e65268302f5fb63
Mahedi205339 <mahedi205339@gmail.com>
2024-06-04 23:27:51 +0600

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
-
+import { Check, Phone, Star } from "lucide-react";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -39,24 +39,55 @@ export default function Home() {
                 </li>
               </div>
 
+
             </ul>
 
             <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
-              <div>
-                <Image src="/monkey1.png" alt="" width={100} height={100} />
+
+              <div className="flex -space-x-4">
+                <Image className="inline-block rounded-full ring-2 ring-slate-100 w-10 h-10"
+                  src="/users/user-2.png"
+                  alt="" width={100} height={100} />
+                <Image className="inline-block rounded-full ring-2 ring-slate-100 w-10 h-10"
+                  src="/users/user-3.png"
+                  alt="" width={100} height={100} />
+                <Image className="inline-block rounded-full ring-2 ring-slate-100 w-10 h-10"
+                  src="/users/user-1.png"
+                  alt="" width={100} height={100} />
+                <Image className="inline-block rounded-full ring-2 ring-slate-100 w-10 h-10"
+                  src="/users/user-4.jpg"
+                  alt="" width={100} height={100} />
+                <Image className="inline-block rounded-full ring-2 ring-slate-100 w-10 h-10 object-cover"
+                  src="/users/user-5.jpg"
+                  alt="" width={100} height={100} />
+
               </div>
-              <h5>Our pleasure</h5>
+
+              {/* stars */}
+              <div className='flex flex-col justify-between items-center sm:items-start'>
+                <div className='flex gap-0.5'>
+                  <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                  <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                  <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                  <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                  <Star className='h-4 w-4 text-green-600 fill-green-600' />
+                </div>
+
+                <p>
+                  <span
+                    className="font-semibold">1.250 </span>
+                  happy customers!
+                </p>
+              </div>
             </div>
-
-            <div>
-              {/* reviews form user */}
-
-
-
-
+          </div>
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <Image src="/your_image.png" className="absolute w-40 lg:w-52 left-56 -top-20 select-none sm:block lg:hidden xl:block" alt="" width={150} height={150} />
+              <Image alt="" src="/line.png" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <Phone/>
 
             </div>
-
 
           </div>
         </MaxWidthWrapper>
---
+++ src/components/Phone.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils';
+import React, { HTMLAttributes } from 'react';
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+    imgSrc: string
+    dark?: boolean
+}
+
+const Phone = ({ className, imgSrc, dark = false, ...props }: PhoneProps) => {
+    return (
+        <div className={cn("relative pointer-events-none z-50 overflow-hidden", className)}>
+
+        </div>
+    );
+};
+
+export default Phone;
