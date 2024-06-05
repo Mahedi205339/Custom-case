d40ba53b0dd874a92a01863bf2f7d8ff47d7427f
Mahedi205339 <mahedi205339@gmail.com>
2024-06-05 11:38:06 +0600

    added Navbar layout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import MaxWidthWrapper from './MaxWidthWrapper';
 import Link from 'next/link';
+import { buttonVariants } from './ui/button';
+import { ArrowRight } from 'lucide-react';
 
 const Navbar = () => {
 
     const user = undefined;
+    const isAdmin = false;
 
     return (
         <nav className='sticky z-[100] h-14 inset-x-0 top-0 w-full border-b bg-gray-200 bg-white/75 backdrop-blur-lg transition-all'>
@@ -16,7 +19,36 @@ const Navbar = () => {
                     <div className='h-full flex items-center space-x-4'>
                         {user ? (
                             <>
-                            <Link href="/api/auth/logout"></Link>
+                                <Link href="/api/auth/logout"
+                                    className={buttonVariants({
+                                        size: 'sm', variant: 'ghost'
+                                    })}
+                                >
+                                    Sign out
+                                </Link>
+
+                                {isAdmin ? <>
+                                    <Link href="/api/auth/logout"
+                                        className={buttonVariants({
+                                            size: 'sm', variant: 'ghost'
+                                        })}
+                                    >
+                                        Dashboard
+                                    </Link>
+                                    <Link href="/configure/upload"
+                                        className={buttonVariants({
+                                            size: 'sm',
+                                             className:"hidden sm:flex items-center gap-1"
+
+                                        })}
+                                    >
+                                        Create Case
+                                        <ArrowRight className='ml-1.5 h-5 w-5' />
+                                    </Link>
+
+                                </>
+                                    : null
+                                }
                             </>
                         ) : (
                             <>
