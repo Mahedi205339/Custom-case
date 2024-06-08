388f3b8ab61dcdc9dd5261ab98790c7f38e70869
Mahedi205339 <mahedi205339@gmail.com>
2024-06-08 09:34:32 +0600

    added testimonial pics in public folder

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -3,9 +3,18 @@
 
 import { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
+import { useInView } from "framer-motion";
+
+const PHONES = [
+    ""
+]
 
 function ReviewGrid() {
     const containerRef = useRef<HTMLDivElement | null>(null)
+
+    const inInView = useInView(containerRef, { once: true, amount: 0.4 })
+
+
     return <div className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"> </div>
 }
 
