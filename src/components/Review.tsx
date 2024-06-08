c245811c64558570a695925c451b51e11286b03c
Mahedi205339 <mahedi205339@gmail.com>
2024-06-08 10:16:37 +0600

    added split array and colomn related function

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -4,18 +4,68 @@
 import { useRef } from "react";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { useInView } from "framer-motion";
+import { cn } from "@/lib/utils";
 
 const PHONES = [
-    ""
+    "/testimonials/1.jpg",
+    "/testimonials/2.jpg",
+    "/testimonials/3.jpg",
+    "/testimonials/4.jpg",
+    "/testimonials/5.jpg",
+    "/testimonials/6.jpg",
 ]
+
+function splitArray<T>(array: Array<T>, numParts: number): Array<Array<T>> {
+    const result: Array<Array<T>> = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const index = i % numParts;
+        if (!result[index]) {
+            result[index] = [];
+        }
+        result[index].push(array[i]);
+    }
+
+    return result;
+}
+
+function ReviewColumn({
+    reviews,
+    className,
+    reviewClassName,
+    msPerPixel = 0
+}:
+    {
+        reviews: string[]
+        className: string
+        reviewClassName?: (reviewIndex: number) => string
+        msPerPixel?: number
+
+    }){
+        const columRef = useRef<HTMLDivElement | null>(null)
+        return <div ref={columRef}
+        className={cn('animate-marquee space-y-8 py-4' , className)}
+        style={{'--marquee-duration':duration} as React.CSSProperties}
+        ></div>
+    }
+
 
 function ReviewGrid() {
     const containerRef = useRef<HTMLDivElement | null>(null)
+    const isInView = useInView(containerRef, { once: true, amount: 0.4 })
+    const columns = splitArray(PHONES, 3)
+    const column1 = columns[0]
+    const column2 = columns[1]
+    const column3 = splitArray(columns[2], 2)
 
-    const inInView = useInView(containerRef, { once: true, amount: 0.4 })
 
 
-    return <div className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3"> </div>
+
+    return <div className="relative -mx-4 mt-16 grid h-[49rem] max-h-[150vh] grid-cols-1 items-start gap-8 overflow-hidden px-4 sm:mt-20 md:grid-cols-2 lg:grid-cols-3">{isInView ?
+        <>
+            <ReviewColumn />
+
+        </> : null} </div>
 }
 
 
