a95d326d61b47fca2ab1577e101126aa8947ef3a
Mahedi205339 <mahedi205339@gmail.com>
2024-06-11 14:24:56 +0600

    added Design Configurator file for editing image

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,4 +1,7 @@
+import { db } from '@/db';
+import { notFound } from 'next/navigation';
 import React from 'react';
+import DesignConfigurator from './DesignConfigurator';
 
 interface PageProps {
     searchParams: {
@@ -6,17 +9,23 @@ interface PageProps {
     }
 }
 
-
 const page = async ({ searchParams }: PageProps) => {
-    //make db call here
+
     const { id } = searchParams
 
-
+    if (!id || typeof id != 'string') {
+        return notFound()
+    }
+    const configuration = await db.configuration.findUnique({
+        where: { id },
+    })
+    if (!configuration) {
+        return notFound()
+    }
+    const { imageUrl, width, height } = configuration
 
     return (
-        <div>
-            {id}
-        </div>
+        <DesignConfigurator/>
     );
 };
 
