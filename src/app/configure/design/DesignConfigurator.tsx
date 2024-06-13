acce9fcde356068183cf1dc85ce7bc32ef9dd764
Mahedi205339 <mahedi205339@gmail.com>
2024-06-13 11:09:55 +0600

    phone back css related problem fixxed

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -31,104 +31,109 @@ const DesignConfigurator = ({ configId,
 
 
 
-    return (
-        <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
-            <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+    return (<div
+        className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
+        <div className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
 
 
-                <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
-                    <AspectRatio ratio={896 / 1831} className='pointer-events-none relative z-50 aspect-[869/1831] w-full'>
-                        <NextImage
-                            fill
-                            alt='phone image'
-                            src='/phone-template.png'
-                            className='pointer-events-none z-50 select-none'
-                        />
-                    </AspectRatio>
-                    <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px first-letter:first-line:  rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
-                    <div className={cn('absolute , left-[3px] top-px right-[3px] bottom-px rounded-[32px]',
-                       `bg-${options.color.tw}`
-                    )} />
+            <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
+                <AspectRatio ratio={896 / 1831} className='pointer-events-none relative z-50 aspect-[869/1831] w-full'>
+                    <NextImage
+                        fill
+                        alt='phone image'
+                        src='/phone-template.png'
+                        className='pointer-events-none z-50 select-none'
+                    />
+                </AspectRatio>
+                <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] -bottom-3 first-letter:first-line:  rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
+                <div className={cn('absolute , left-[3px] top-px right-[3px] -bottom-3 rounded-[32px]',
+                    `bg-${options.color.tw}`
+                )} />
 
-                </div>
-                <Rnd default={{
-                    x: 150,
-                    y: 205,
-                    height: imageDimensions.height / 8,
-                    width: imageDimensions.width / 8,
-                }}
-                    lockAspectRatio
-                    className='absolute z-20 border-[3px] border-primary'
-                    resizeHandleComponent={{
-                        bottomRight: <HandleComponents />,
-                        bottomLeft: <HandleComponents />,
-                        topRight: <HandleComponents />,
-                        topLeft: <HandleComponents />,
-
-                    }}
-                >
-                    <div className='relative w-full h-full'>
-                        <NextImage src={imageUrl}
-                            fill
-                            alt='your image'
-                            className='pointer-events-none'
-                        />
-                    </div>
-                </Rnd>
             </div>
+            <Rnd default={{
+                x: 150,
+                y: 205,
+                height: imageDimensions.height / 8,
+                width: imageDimensions.width / 8,
+            }}
+                lockAspectRatio
+                className='absolute z-20 border-[3px] border-primary'
+                resizeHandleComponent={{
+                    bottomRight: <HandleComponents />,
+                    bottomLeft: <HandleComponents />,
+                    topRight: <HandleComponents />,
+                    topLeft: <HandleComponents />,
 
-            <div className='h-[37.5rem] flex flex-col bg-white'>
-                <ScrollArea className='relative flex-1 overflow-auto'>
-                    <div aria-hidden="true" className='absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t-from-white pointer-events-none ' />
+                }}
+            >
+                <div className='relative w-full h-full'>
+                    <NextImage src={imageUrl}
+                        fill
+                        alt='your image'
+                        className='pointer-events-none'
+                    />
+                </div>
+            </Rnd>
+        </div>
 
-                    <div className='px-8 pb-12 pt-8'>
-                        <h2 className='tracking-tight font-bold text-3xl'>
-                            Customize your case!
-                        </h2>
-                        <div className='w-full h-px bg-zinc-200 my-6' />
-                        <div className='relative mt-4 h-full flex flex-col justify-between'>
+        <div className='h-[37.5rem] flex flex-col bg-white'>
+            <ScrollArea className='relative flex-1 overflow-auto'>
+                <div aria-hidden="true" className='absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t-from-white pointer-events-none ' />
+
+                <div className='px-8 pb-12 pt-8'>
+                    <h2 className='tracking-tight font-bold text-3xl'>
+                        Customize your case!
+                    </h2>
+                    <div className='w-full h-px bg-zinc-200 my-6' />
+                    <div className='relative mt-4 h-full flex flex-col justify-between'>
+                        <div className='flex flex-col gap-6' >
+
                             <RadioGroup value={options.color}
                                 onChange={(val) => {
                                     setOptions((prev) => ({
                                         ...prev,
                                         color: val,
                                     }))
-                                }}
-                            >
+                                }}  >
+
                                 <Label>
                                     Color:{options.color.label}
                                 </Label>
                                 <div className='mt-3 flex items-center space-x-3'>
-                                    {
-                                        COLORS.map((color) => (
-                                            <RadioGroup.Option
-                                                key={color.label}
-                                                value={color}
-                                                className={({ active, checked }) =>
-                                                    cn(
-                                                        'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent',
-                                                        {
-                                                            [`border-${color.tw}`]: active || checked,
-                                                        }
-                                                    )
-                                                }>
-                                                <span
-                                                    className={cn(
-                                                        `bg-${color.tw}`,
-                                                        'h-8 w-8 rounded-full border border-black border-opacity-10'
-                                                    )}
-                                                />
-                                            </RadioGroup.Option>
-                                        ))
-                                    }
+                                    {COLORS.map((color) => (
+                                        <RadioGroup.Option
+                                            key={color.label}
+                                            value={color}
+                                            className={({ active, checked }) =>
+                                                cn(
+                                                    'relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent',
+                                                    {
+                                                        [`border-${color.tw}`]: active || checked,
+                                                    }
+                                                )
+                                            }>
+                                            <span
+                                                className={cn(
+                                                    `bg-${color.tw}`,
+                                                    'h-8 w-8 rounded-full border border-black border-opacity-10'
+                                                )}
+                                            />
+                                        </RadioGroup.Option>))}
                                 </div>
                             </RadioGroup>
+
+                            <div className='relative flex flex-col gap-3 w-full'>
+                                <Label>Model</Label>
+
+                            </div>
                         </div>
                     </div>
-                </ScrollArea>
-            </div>
-        </div >
-    );
+                </div>
+            </ScrollArea>
+        </div>
+
+    </div >);
 };
 
 export default DesignConfigurator;
