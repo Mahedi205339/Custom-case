0e8df618c8242825ee9ca0a904730bd56a3ef742
Mahedi205339 <mahedi205339@gmail.com>
2024-06-13 12:33:23 +0600

    added check icon for selected model

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -9,8 +9,11 @@ import { ScrollArea } from '@/components/ui/scroll-area';
 import { RadioGroup } from "@headlessui/react"
 import { COLORS, MODELS } from '@/validators/option-validator';
 import { Label } from '@/components/ui/label';
-import { DropdownMenu, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/button';
+import { Check, ChevronsUpDown } from 'lucide-react';
+import PreviousMap from 'postcss/lib/previous-map';
+import { late } from 'zod';
 
 interface DesignConfiguratorProps {
     configId: string
@@ -138,10 +141,30 @@ const DesignConfigurator = ({ configId,
                                             role='combobox'
                                         >
                                             {options.model.label}
-
+                                            <ChevronsUpDown
+                                                className='ml-2 h-4 w-4 shrink-0 opacity-50'
+                                            />
                                         </Button>
-
                                     </DropdownMenuTrigger>
+                                    <DropdownMenuContent>
+                                        {MODELS.options.map((model) => (
+                                            <DropdownMenuItem key={model.label}
+                                                className={cn("flex text-sm gap-1 items-center p-1.5 cursor-default hover:bg-zinc-100", {
+                                                    "bg-zinc-100": options.model.label,
+                                                })}
+                                                onClick={() => {
+                                                    setOptions((prev) => ({ ...prev, model }))
+                                                }}
+                                            >
+                                                <Check
+                                                className={cn('mr-2 h-4 w-4',
+                                                    model.label === options.model.label? "opacity-100":'opacity-0'
+                                                )}
+                                                />
+                                                {model.label}
+                                            </DropdownMenuItem>
+                                        ))}
+                                    </DropdownMenuContent>
                                 </DropdownMenu>
 
                             </div>
@@ -149,7 +172,7 @@ const DesignConfigurator = ({ configId,
                     </div>
                 </div>
             </ScrollArea>
-        </div>
+        </div >
 
     </div >);
 };
