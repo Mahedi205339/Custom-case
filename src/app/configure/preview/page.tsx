0c3910f2fbc688ffa42ff3ee23fb8879718ef7c3
Mahedi205339 <mahedi205339@gmail.com>
2024-06-20 19:57:00 +0600

    added some changes in config preview page

--- src/app/configure/preview/page.tsx
+++ src/app/configure/preview/page.tsx
@@ -1,3 +1,4 @@
+import { db } from '@/db';
 import { notFound } from 'next/navigation';
 import React from 'react';
 
@@ -7,12 +8,16 @@ interface PageProps {
     }
 }
 
-const page = ({ searchParams }: PageProps) => {
- 
-    const {id} = searchParams  ;
-    if(!id|| typeof id !== 'string'){
+const page = async ({ searchParams }: PageProps) => {
+
+    const { id } = searchParams;
+    if (!id || typeof id !== 'string') {
         return notFound()
     }
+
+    const configuration = await db.configuration.findUnique({
+     where:{id},
+    })
 
     return (
         <div>
