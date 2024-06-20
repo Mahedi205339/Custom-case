2edade1a5333a84e3debbf0dbf139ee9a0e9e33c
Mahedi205339 <mahedi205339@gmail.com>
2024-06-20 19:55:51 +0600

    added preview page

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,24 @@
+import { notFound } from 'next/navigation';
+import React from 'react';
+
+interface PageProps {
+    searchParams: {
+        [key: string]: string | string[] | undefined;
+    }
+}
+
+const page = ({ searchParams }: PageProps) => {
+ 
+    const {id} = searchParams  ;
+    if(!id|| typeof id !== 'string'){
+        return notFound()
+    }
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default page;
