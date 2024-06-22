8183b776fff28f3ec75b46d585ddb2ff55e07c55
Mahedi205339 <mahedi205339@gmail.com>
2024-06-22 14:39:40 +0600

    added grainy back effect on css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Recursive } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Recursive({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Custom Case",
