33a1eb27a7bf33da3fe3878d71dd1f2f140a9093
Mahedi205339 <mahedi205339@gmail.com>
2024-06-23 14:57:25 +0600

    added stripe payment gatway

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -15,8 +15,9 @@ import { useToast } from '@/components/ui/use-toast';
 
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
-     const router = useRouter()
-     const {toast} = useToast()
+    const router = useRouter()
+    const { toast } = useToast()
+
     const [showConfetti, setShowConfetti] = useState(false);
     useEffect(() => {
         setShowConfetti(true)
@@ -39,21 +40,21 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
     // console.log(formatePrice(PRODUCT_PRICES.finish.textured /100))
     // console.log(formatePrice(PRODUCT_PRICES.materials.polycarbonate))
 
-    const {} = useMutation({
-        mutationKey:["get-checkout-session"],
-        mutationFn:createCheckoutSession ,
-        onSuccess:({url})=>{
-            if(url) router.push(url)
-                else throw new Error('Unable to retrieve payment URL')
+    const { mutate: createPaymentSession } = useMutation({
+        mutationKey: ["get-checkout-session"],
+        mutationFn: createCheckoutSession,
+        onSuccess: ({ url }) => {
+            if (url) router.push(url)
+            else throw new Error('Unable to retrieve payment URL')
         },
-        onError: () =>{
-      toast({
-        title:'Something went wrong',
-        description:'There was an error on our end. Please try again',
-        variant:'destructive'
-      })
+        onError: () => {
+            toast({
+                title: 'Something went wrong',
+                description: 'There was an error on our end. Please try again',
+                variant: 'destructive'
+            })
         }
-       })
+    })
 
     return (
         <>
@@ -114,7 +115,7 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
                                     <div className='flex items-center justify-between py-1 mt-2'>
                                         <p className='text-gray-600'>Textured finish</p>
                                         <p className='font-medium text-gray-900'>
-                                            {formatePrice(PRODUCT_PRICES.finish.textured /100)}
+                                            {formatePrice(PRODUCT_PRICES.finish.textured / 100)}
 
                                         </p>
                                     </div>
@@ -142,10 +143,10 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
 
                         <div className='mt-8 flex justify-end pb-12'>
                             <Button
-                            // isLoading={true}
-                            // disabled={true}
-                            // loadingText='loading'
-                                // onClick={() => handleCheckout()}
+                                isLoading={true}
+                                disabled={true}
+                                loadingText='loading'
+                                onClick={() => createPaymentSession({ configId: configuration.id })}
                                 className='px-4 sm:px-6 lg:px-8'>
                                 Check out <ArrowRight className='h-4 w-4 ml-1.5 inline' />
                             </Button>
