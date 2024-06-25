1144cb74becf527102a9e827f838d54e8b64f586
Mahedi205339 <mahedi205339@gmail.com>
2024-06-25 23:33:32 +0600

    added user data storing function with laoding state

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -3,8 +3,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { getAuthStatus } from "./action";
+import { useRouter } from "next/navigation";
+import { Loader2 } from "lucide-react";
 
 const Page = () => {
+  const router = useRouter();
 
   const [configId, setConfigId] = useState<string | null>(null);
   useEffect(() => {
@@ -12,7 +15,7 @@ const Page = () => {
     if (configurationId) setConfigId(configurationId);
 
   }, []);
-  const { } = useQuery({
+  const { data } = useQuery({
     queryKey: ["auth-callback"],
     queryFn: async () => getAuthStatus(),
 
@@ -22,9 +25,23 @@ const Page = () => {
     retryDelay: 500,
   })
 
+  if (data?.success) {
+    if (configId) {
+      localStorage.removeItem("configurationId")
+      router.push(`/configure/preview?id=${configId}`)
+    } else {
+      router.push('/')
+    }
+  }
+
 
   return (
-    <div>
+    <div className="w-full flex mt-24 justify-center">
+      <div className="flex flex-col items-center gap-2">
+        <Loader2 className="h-8 w-8 animate-spin text-zinc-500" />
+        <h3 className="font-semibold text-xl">Logging you in..</h3>
+        <p>You will ve redirected automatically</p>
+      </div>
 
     </div>
   );
