c1cd8141d6be03d81e230618256c89790c43b2e4
Mahedi205339 <mahedi205339@gmail.com>
2024-06-28 11:36:36 +0600

    added some extra chnages for debugging

--- src/components/PhonePreview.tsx
+++ src/components/PhonePreview.tsx
@@ -1,58 +1,75 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/alt-text */
 'use client'
 
-import { cn } from "@/lib/utils";
-import { CaseColor } from "@prisma/client";
-import { AspectRatio } from "@radix-ui/react-aspect-ratio";
-import { useEffect, useRef, useState } from "react";
+import { CaseColor } from '@prisma/client'
+import { useEffect, useRef, useState } from 'react'
+import { AspectRatio } from './ui/aspect-ratio'
+import { cn } from '@/lib/utils'
 
-const PhonePreview = ({ croppedImageUrl, color }: {
-    croppedImageUrl: string
-    color: CaseColor
+const PhonePreview = ({
+  croppedImageUrl,
+  color,
+}: {
+  croppedImageUrl: string
+  color: CaseColor
 }) => {
-    const ref = useRef<HTMLDivElement>(null)
+  const ref = useRef<HTMLDivElement>(null)
 
-    const [renderDimensions, setRenderDimensions] = useState({
-        height: 0,
-        width: 0,
-    })
+  const [renderedDimensions, setRenderedDimensions] = useState({
+    height: 0,
+    width: 0,
+  })
 
-    const handleResize = () => {
-        if(!ref.current) return 
-        const {width , height} = ref.current.getBoundingClientRect()
-        setRenderDimensions({width , height})
-     
-    }
-    useEffect(() => {
-        handleResize()
-        window.addEventListener("resize", handleResize)
-        return () => window.addEventListener("resize" ,handleResize)
-    }, [ref.current])
+  const handleResize = () => {
+    if (!ref.current) return
+    const { width, height } = ref.current.getBoundingClientRect()
+    setRenderedDimensions({ width, height })
+  }
 
-    let caseBackgroundColor = 'bg-zinc-950'
-    if (color === 'blue') caseBackgroundColor = 'bg-blue-950'
-    if (color === 'rose') caseBackgroundColor = 'bg-rose-950'
-    if (color === 'cyan') caseBackgroundColor = 'bg-cyan-950'
-    if (color === 'green') caseBackgroundColor = 'bg-green-950'
+  useEffect(() => {
+    handleResize()
 
-    return (
-        <AspectRatio
-            ref={ref}
-            ratio={3000 / 2001}
-            className="relative"
-        >
-            <div className="absolute z-20 scale-[1.0536]" style={{ left: renderDimensions.width / 2 - renderDimensions.width / (1216 - 121), top: renderDimensions.height / 6.22, }}>
-                <img width={renderDimensions.width / (3000 / 637)} className={cn('phone-skew relative  z-20 rounded-t-[15px] rounded-b-[10px] md:rounded-t-[30px] md:rounded-b-[20px]',
-                    caseBackgroundColor)} src={croppedImageUrl} alt="" />
-            </div>
-            <div className="relative h-full w-full z-40">
-                <img src="/clearphone.png" alt="phone"
-                    className="pointer-events-none h-full w-full antialiased rounded-md"
-                />
+    window.addEventListener('resize', handleResize)
 
-            </div>
-        </AspectRatio>
-    );
-};
+    return () => window.removeEventListener('resize', handleResize)
+  }, [ref.current])
 
-export default PhonePreview;
+  let caseBackgroundColor = 'bg-zinc-900'
+  if (color === 'blue') caseBackgroundColor = 'bg-blue-950'
+  if (color === 'rose') caseBackgroundColor = 'bg-rose-950'
+  if (color === 'cyan') caseBackgroundColor = 'bg-cyan-950'
+  if (color === 'green') caseBackgroundColor = 'bg-green-950'
+
+  return (
+    <AspectRatio ref={ref} ratio={3000 / 2001} className='relative'>
+      <div
+        className='absolute z-20 scale-[1.0352]'
+        style={{
+          left:
+            renderedDimensions.width / 2 -
+            renderedDimensions.width / (1216 / 121),
+          top: renderedDimensions.height / 6.22,
+        }}>
+        <img
+          width={renderedDimensions.width / (3000 / 637)}
+          className={cn(
+            'phone-skew relative z-20 rounded-t-[15px] rounded-b-[10px] md:rounded-t-[30px] md:rounded-b-[20px]',
+            caseBackgroundColor
+          )}
+          src={croppedImageUrl}
+        />
+      </div>
+
+      <div className='relative h-full w-full z-40'>
+        <img
+          alt='phone'
+          src='/clearphone.png'
+          className='pointer-events-none h-full w-full antialiased rounded-md'
+        />
+      </div>
+    </AspectRatio>
+  )
+}
+
+export default PhonePreview
